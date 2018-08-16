b8925d0f3d5bb03530349f8bfab143bb6f2baefe
Rebecca Strauss <rstrauss127@gmail.com>
2018-08-16 23:22:46 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,9 @@
 class School
 
+  attr_reader :name
+  
   def initialize(name)
     @name = name
-    @roster = Hash.new
   end
 
-  def add_student(student, grade)
-
-    if @roster.has_key?(grade)
-      @roster[grade] << student
-    else
-      @roster[grade] = Array.new
-      @roster[grade] << student
-    end
-
-  end
-
-  def roster #reader for roster
-    @roster
-  end
-
-  def grade(grade)
-    @roster[grade]
-  end
-
-  def sort
-    @roster.each do |key, value|
-      value.sort!
-    end
-  end
 end
